64d98ae2c2b62a388efb17d1fd3ac8386ca2c06c
Jain <anjajain@deloitte.com>
2021-05-26 15:59:21 +0530

    Initial setup for Green Earth app

---
+++ Green-Earth-app/main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow () {
+  const win = new BrowserWindow({
+    title: "Carbon Footprint",
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+app.on('ready',createWindow);
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
